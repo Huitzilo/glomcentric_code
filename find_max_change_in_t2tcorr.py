f160f043e6bed9226111a50f8b67f8a6d8e2a590
Michael Schmuker <m.schmuker@biomachinelearning.net>
2014-11-17 13:11:43 +0000

    Find optimal sparseness factor:first version

---
+++ find_max_change_in_t2tcorr.py
@@ -0,0 +1,47 @@
+from configobj import ConfigObj
+import os
+import numpy as np
+import sys
+import regnmf.ImageAnalysisComponents as ia
+from scipy.spatial.distance import pdist
+
+# this script should be called in the directory where the data is. 
+# results are saved to that same directory.
+
+basepath = os.path.abspath(sys.argv[1])
+datafilename =  'ios_meas'
+response_window = (3,5) #define frames to calculate odor response; ios:(3,5) sph:(8,12
+
+
+#from config file nnmf_150_sm2_convex.ini
+config_dict = {
+'num_components':150,
+'sparse_fct':'global_sparse',
+'smooth_param':2,
+'init':'convex',
+'neg_time':False,
+'verbose':0,
+'maxcount':50#,
+#'sparse_param':sparseness
+}
+
+print("analysing files in {}".format(basepath))
+for sparseness in np.arange(0.1,1.000001,0.1):
+    ts_path = os.path.join(basepath, '_'.join(["nnmf", 
+                                               str(config_dict['num_components']), 
+                                               "sm{}".format(config_dict['smooth_param']), 
+                                               config_dict['init'],
+                                               'sp{:02.0f}'.format(sparseness*10),
+                                               datafilename]))
+    
+    decomposition = ia.TimeSeries()
+    decomposition.load(ts_path)
+    signal = ia.TrialMean()(ia.CutOut(response_window)(decomposition))
+    mode_cor = ia.CalcStimulusDrive()(signal)
+    mask = mode_cor._series.squeeze()<0.5
+    if np.sum(mask) > 1: #if there are stimulus driven components  
+        selected_modes = ia.SelectObjects()(decomposition, mask)   
+        cor = np.nanmax(1-pdist(selected_modes.base._series, 'correlation'))
+    else:
+        cor = np.nanmax(1-pdist(decomposition.base._series, 'correlation'))
+    print("{}\t{}".format(sparseness, cor))
